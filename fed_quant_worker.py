77e2c99d49e84799dc295c4c56c74eec5e5720cc
cyy <cyyever@outlook.com>
2021-01-23 16:49:23 +0700

    add code

--- fed_quant_worker.py
+++ fed_quant_worker.py
@@ -4,7 +4,7 @@ import sys
 
 import torch
 from cyy_naive_lib.log import get_logger
-from cyy_naive_pytorch_lib.device import get_cpu_device
+from cyy_naive_pytorch_lib.device import get_cpu_device, get_device
 from cyy_naive_pytorch_lib.inference import Inferencer
 from cyy_naive_pytorch_lib.ml_types import MachineLearningPhase
 from cyy_naive_pytorch_lib.model_util import ModelUtil
@@ -56,58 +56,68 @@ class FedQuantWorker(Worker):
         if epoch % self.local_epoch != 0:
             return
 
-        trainer.model.eval()
+        # for k, v in trainer.model.quant.named_buffers():
+        #     print("buffer k=", k)
+        #     print("buffer v=", v)
+
         trainer.model.cpu()
-        get_logger().info(trainer.model)
-
+        trainer.model.eval()
         quantized_model: torch.nn.Module = torch.quantization.convert(trainer.model)
+        get_logger().info("quantized_model is %s",quantized_model)
 
-        trainer.set_model(quantized_model)
-        trainer.train(device=get_cpu_device())
-        sys.exit(0)
         state_dict = quantized_model.state_dict()
 
-        model_util = ModelUtil(trainer.model)
+        model_util = ModelUtil(self.original_model)
         for k in state_dict.keys():
-            # if k.startswith("quant."):
-            #     continue
+            prefix = "module."
+            if not k.startswith(prefix):
+                continue
             v = state_dict[k]
+            k = k[len(prefix):]
+            print("k=", k)
             if not isinstance(v, torch.Tensor):
                 continue
+
             if v.is_quantized:
-                get_logger().info("for quantization k=%s", k)
                 v = v.int_repr().float()
-                print(v.shape)
+                # get_logger().info("for quantization k=%s", k)
+            # else:
+            # get_logger().info("for not quantization k=%s", k)
+            if model_util.has_attr(k):
+                model_util.set_attr(k, v)
+                get_logger().info("set value for k=%s", k)
             else:
-                get_logger().info("for not quantization k=%s", k)
+                get_logger().info("no value for k=%s", k)
 
-            # if model_util.has_attr(k):
-            #     model_util.set_attr(k, v)
-            # # prefix = "module."
-            # # if k.startswith(prefix):
-            # #     k = k[len(prefix):]
-            # # get_logger().info(k)
-            # # if not model_util.has_attr(k):
-            #     continue
-            # model_util.set_attr(k, v)
+        self.__prepare_quantization()
+        model_util = ModelUtil(self.trainer.model)
+        device = kwargs.get("device")
+        for k in state_dict.keys():
+            print("k=", k)
+            v = state_dict[k]
+            if k == "quant.scale":
+                fake_quant = model_util.get_attr("quant.activation_post_process")
+                fake_quant.register_buffer("scale", v)
+                # get_logger().info("register scale %s", v)
+                continue
+            if k == "quant.zero_point":
+                fake_quant = model_util.get_attr("quant.activation_post_process")
+                fake_quant.register_buffer("zero_point", v)
+                get_logger().info("register zero_point %s", v)
+                continue
 
-        # quantized_model.load_state_dict(state_dict)
-        # trainer.set_model(quantized_model)
-        # device = kwargs.get("device")
-        sys.exit(0)
-        res = trainer.get_inferencer(
+        res = self.trainer.get_inferencer(
             MachineLearningPhase.Test, copy_model=False
-        ).inference(device=get_cpu_device())
-        get_logger().info(res)
+        ).inference(device=device)
+        get_logger().info("aaaaaaaaaaaaaaaaa=", res)
+        optimizer: torch.optim.Optimizer = kwargs.get("optimizer")
+        optimizer.param_groups.clear()
+        optimizer.add_param_group({"params": self.trainer.model.parameters()})
 
-        sys.exit(0)
         return
 
         # self.__prepare_quantization()
         # self.trainer.set_model(self.original_model)
-        optimizer: torch.optim.Optimizer = kwargs.get("optimizer")
-        optimizer.param_groups.clear()
-        optimizer.add_param_group({"params": trainer.model.parameters()})
 
         # get_logger().info("aggregate parameters at epoch %s", epoch)
         # self.server.add_parameter(model_util.get_parameter_list())
