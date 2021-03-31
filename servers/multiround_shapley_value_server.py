b11d3a9370da5f188096cca66db1c396a271c4d2
cyy <cyyever@outlook.com>
2021-03-31 21:47:06 +0800

    add code

--- servers/multiround_shapley_value_server.py
+++ servers/multiround_shapley_value_server.py
@@ -1,5 +1,7 @@
 import math
 
+from cyy_naive_lib.log import get_logger
+
 from .shapley_value_server import ShapleyValueServer
 
 
@@ -7,13 +9,32 @@ class MultiRoundShapleyValueServer(ShapleyValueServer):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.shapley_values = dict()
+        self.round_trunc_threshold = kwargs.get("round_trunc_threshold", None)
 
     def _process_aggregated_parameter(self, aggregated_parameter: dict):
         metrics = dict()
+        if self.round_trunc_threshold is not None:
+            last_round_metric = self.get_metric(self._prev_model)
+            this_round_metric = self.get_metric(aggregated_parameter)
+            metrics[()] = last_round_metric
+            metrics[tuple(sorted(range(self.worker_number)))] = this_round_metric
+            if abs(this_round_metric - last_round_metric) <= self.round_trunc_threshold:
+                get_logger().warning(
+                    "this_round_metric %s last_round_metric %s",
+                    this_round_metric,
+                    last_round_metric,
+                )
+                self.shapley_values[self.round] = {
+                    i: 0 for i in range(self.worker_number)
+                }
+                return aggregated_parameter
+
         for subset in self.powerset(range(self.worker_number)):
-            subset_model = self.get_subset_model(subset, self._prev_model)
-            metric = self.get_metric(subset_model)
-            metrics[tuple(sorted(subset))] = metric
+            key = tuple(sorted(subset))
+            if key not in metrics:
+                subset_model = self.get_subset_model(subset, self._prev_model)
+                metric = self.get_metric(subset_model)
+                metrics[key] = metric
 
         round_shapley_values = dict()
         for subset, metric in metrics.items():
@@ -30,5 +51,6 @@ class MultiRoundShapleyValueServer(ShapleyValueServer):
                     * self.worker_number
                 )
 
+        get_logger().error("round_shapley_values %s", round_shapley_values)
         self.shapley_values[self.round] = round_shapley_values
         return aggregated_parameter
